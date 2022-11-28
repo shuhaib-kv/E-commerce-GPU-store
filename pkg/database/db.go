43b52dd9996906669f7d4bfcf42b794b36c324e8
shuhaib <kvshuhaib2@gmail.com>
2022-11-28 17:54:19 +0530

    database id

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -12,10 +12,9 @@ var Db *gorm.DB
 func ConnectDB() {
 	var err error
 	// dsn := os.Getenv("DB")
-	Db, err = gorm.Open(postgres.Open("host=localhost user=soib password=soib dbname=gpu_ecom port=5432 sslmode=prefer"), &gorm.Config{})
+	Db, err = gorm.Open(postgres.Open("host=localhost user=postgres  dbname=gpu_ecom port=5432 "), &gorm.Config{})
 	if err != nil {
 		fmt.Println("error", err)
 	}
-	fmt.Printf("Database Type ='%s'\n Connected to database sussesfully!", Db.Name())
 
 }
