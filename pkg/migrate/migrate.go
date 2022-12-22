c91f2b0f2218ad453e80dca90653db068344999f
shuhaib <kvshuhaib2@gmail.com>
2022-12-22 09:05:35 +0530

    docker set up

--- pkg/migrate/migrate.go
+++ pkg/migrate/migrate.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"ga/pkg/database"
-	"ga/pkg/models"
 )
 
 func init() {
@@ -10,22 +9,5 @@ func init() {
 }
 
 func main() {
-	database.Db.AutoMigrate(
-		&models.Users{},
-		&models.Admin{},
-		&models.Product{},
-		&models.Category{},
-		&models.Cart{},
-		&models.Orders{},
-		&models.Address{},
-		&models.Paymentmethod{},
-		&models.Ordereditems{},
-		&models.CartInfo{},
-		&models.RazorPay{},
-		&models.Discount{},
-		&models.Coupon{},
-		&models.Wallet{},
-		&models.Wallethistory{},
-	)
 
 }
