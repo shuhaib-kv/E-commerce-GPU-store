a8293e930fef6f2a46a255d7f1c3639ee102db5e
shuhaib <kvshuhaib2@gmail.com>
2023-01-22 00:02:35 +0530

    routes  update

--- pkg/routes/routes_user.go
+++ pkg/routes/routes_user.go
@@ -18,35 +18,25 @@ func UserRoutes(User *gin.Engine) {
 	//products
 	User.GET("/user/viewproducts", middleware.UserAuth(), controllers.ViewProductsUser)
 	User.GET("/user/viewproducts/:id", middleware.UserAuth(), controllers.ViewProductsUserbyid)
-
-	//category
 	User.GET("/user/product/viewbycategory/:id", middleware.UserAuth(), controllers.ViewProductByCategory)
-	//cart
-	// User.GET("/cart", middleware.UserAuth(), controllers.ViewCart)
-	// User.GET("/cart/add", middleware.UserAuth(), controllers.ViewCart)
-
-	// User.POST("/order/:id", middleware.UserAuth(), controllers.Order)
+	//user profile
 	User.POST("/user/add/address", middleware.UserAuth(), controllers.AddAddress)
 	User.PATCH("/user/edit/address/:id", middleware.UserAuth(), controllers.EditAddress)
-
-	// User.GET("/user/cart/view", middleware.UserAuth(), controllers.CartLists)
-	User.GET("/payment-success", middleware.UserAuth(), controllers.RazorpaySuccess)
-	User.GET("/success", controllers.Success)
 	User.GET("/user/address", middleware.UserAuth(), controllers.ShowAddress)
-
-	//ordernow
-	// User.POST("/user/buynow", middleware.UserAuth(), controllers.OrderNow)
+	// order
 	User.GET("/user/orderview", middleware.UserAuth(), controllers.OrderedItems)
-	// User.GET("/user/order/details/:id", middleware.UserAuth(), controllers.VieworderDetails)
-	// // User.POST("/user/cart/order", middleware.UserAuth(), controllers.BuyFromCart)
 	User.PATCH("/user/orde/cancel", middleware.UserAuth(), controllers.CancelOrder)
-	User.POST("/cart/add", middleware.UserAuth(), controllers.AddTOcart) //new
+	//payment
+	User.GET("/payment-success", middleware.UserAuth(), controllers.RazorpaySuccess)
 	User.GET("/razorpay", middleware.UserAuth(), controllers.RazorPay)
+	User.GET("/success", controllers.Success)
+	//ordernow
+	User.POST("/cart/add", middleware.UserAuth(), controllers.AddTOcart) //new
 	User.GET("/user/cart/view", middleware.UserAuth(), controllers.CartList)
-	User.POST("/chechout", middleware.UserAuth(), controllers.CheckoutCart) //new
+
+	User.POST("/chechout", middleware.UserAuth(), controllers.CheckoutCart)
+	//wallet
 	User.GET("/user/wallet/history", middleware.UserAuth(), controllers.WalletInfo)
 	User.GET("/user/wallet/balance", middleware.UserAuth(), controllers.WalletBalance)
-
-	// User.POST("/od1", middleware.UserAuth(), controllers.CartCheckoutDetails)
 
 }
